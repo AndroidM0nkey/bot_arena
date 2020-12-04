66dc16e4dc92a6b2c78dc91b3f4122052dfbb5f2
Alexander Korzun <sahhash33@gmail.com>
2020-12-04 21:43:15 +0300

    Events: Change name format (PascalCase -> ALL_UPPER) to make tests pass
    In event.py and the corresponding part of the test suit,
    the format of names of cases of the Event algebraic data type
    was changed from PascalCase to ALL_UPPERCASE, so that the `adt`
    library can work with them properly. For more information on this
    problem, see the previous commit message.

--- bot_arena_proto/bot_arena_proto/event.py
+++ bot_arena_proto/bot_arena_proto/event.py
@@ -8,17 +8,17 @@ from adt import adt, Case
 
 @adt
 class Event:
-    SnakeDied: Case[str]
-    GameFinished: Case
-    GameStarted: Case[int, int]
+    SNAKE_DIED: Case[str]
+    GAME_FINISHED: Case
+    GAME_STARTED: Case[int, int]
 
     def to_primitive(self) -> Primitive:
         return cast(
             Primitive,
             self.match(
-                snakedied = lambda snake_name: ['SnakeDied', {'name': snake_name}],
-                gamefinished = lambda: ['GameFinished'],
-                gamestarted = lambda width, height: ['GameStarted', {'width': width, 'height': height}],
+                snake_died = lambda snake_name: ['SnakeDied', {'name': snake_name}],
+                game_finished = lambda: ['GameFinished'],
+                game_started = lambda width, height: ['GameStarted', {'width': width, 'height': height}],
             )
         )
 
@@ -31,14 +31,14 @@ class Event:
             [data_dict] = data
             data_dict = ensure_type(data_dict, dict)
             snake_name = ensure_type(data_dict['name'], str)
-            return Event.SnakeDied(snake_name)
+            return Event.SNAKE_DIED(snake_name)
         if tag == 'GameFinished':
-            return Event.GameFinished()
+            return Event.GAME_FINISHED()
         if tag == 'GameStarted':
             [data_dict] = data
             data_dict = ensure_type(data_dict, dict)
             width = ensure_type(data_dict['width'], int)
             height = ensure_type(data_dict['height'], int)
-            return Event.GameStarted(width, height)
+            return Event.GAME_STARTED(width, height)
 
         raise DeserializationAdtTagError(Class, tag)
--- bot_arena_proto/tests/test_event.py
+++ bot_arena_proto/tests/test_event.py
@@ -4,22 +4,22 @@ import pytest
 
 
 def test_basic_soundness():
-    assert Event.SnakeDied('Bob') == Event.SnakeDied('Bob')
-    assert Event.SnakeDied('Bob') != Event.SnakeDied('Emily')
-    assert Event.SnakeDied('Bob') != Event.GameFinished()
-    assert Event.GameFinished() == Event.GameFinished()
-    assert Event.SnakeDied('A') != Event.SnakeDied('a')
-    assert Event.GameStarted(10, 20) == Event.GameStarted(10, 20)
-    assert Event.GameStarted(10, 20) != Event.GameStarted(20, 10)
+    assert Event.SNAKE_DIED('Bob') == Event.SNAKE_DIED('Bob')
+    assert Event.SNAKE_DIED('Bob') != Event.SNAKE_DIED('Emily')
+    assert Event.SNAKE_DIED('Bob') != Event.GAME_FINISHED()
+    assert Event.GAME_FINISHED() == Event.GAME_FINISHED()
+    assert Event.SNAKE_DIED('A') != Event.SNAKE_DIED('a')
+    assert Event.GAME_STARTED(10, 20) == Event.GAME_STARTED(10, 20)
+    assert Event.GAME_STARTED(10, 20) != Event.GAME_STARTED(20, 10)
 
 
 def test_basic_serde():
-    assert Event.SnakeDied('Mike') == Event.from_primitive(['SnakeDied', {'name': 'Mike'}])
-    assert Event.SnakeDied('Mike') != Event.from_primitive(['SnakeDied', {'name': 'Mike1'}])
-    assert Event.SnakeDied('Mike').to_primitive() == ['SnakeDied', {'name': 'Mike'}]
+    assert Event.SNAKE_DIED('Mike') == Event.from_primitive(['SnakeDied', {'name': 'Mike'}])
+    assert Event.SNAKE_DIED('Mike') != Event.from_primitive(['SnakeDied', {'name': 'Mike1'}])
+    assert Event.SNAKE_DIED('Mike').to_primitive() == ['SnakeDied', {'name': 'Mike'}]
 
-    assert Event.GameFinished() == Event.from_primitive(['GameFinished'])
-    assert Event.GameFinished().to_primitive() == ['GameFinished']
+    assert Event.GAME_FINISHED() == Event.from_primitive(['GameFinished'])
+    assert Event.GAME_FINISHED().to_primitive() == ['GameFinished']
 
-    assert Event.GameStarted(20, 15) == Event.from_primitive(['GameStarted', {'width': 20, 'height': 15}])
-    assert Event.GameStarted(20, 15).to_primitive() == ['GameStarted', {'width': 20, 'height': 15}]
+    assert Event.GAME_STARTED(20, 15) == Event.from_primitive(['GameStarted', {'width': 20, 'height': 15}])
+    assert Event.GAME_STARTED(20, 15).to_primitive() == ['GameStarted', {'width': 20, 'height': 15}]
