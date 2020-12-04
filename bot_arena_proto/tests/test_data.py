bd85955b55e1b1fdd73ea2a97dfa25996487c453
Alexander Korzun <sahhash33@gmail.com>
2020-12-04 22:29:24 +0300

    Expand test suit for bot_arena_proto.data

--- bot_arena_proto/tests/test_data.py
+++ bot_arena_proto/tests/test_data.py
@@ -27,6 +27,11 @@ class TestBasicSoundnesss:
     def test_object():
         assert Object.FOOD() == Object.FOOD()
 
+    @staticmethod
+    def test_action():
+        assert Action.MOVE(Direction.UP()) == Action.MOVE(Direction.UP())
+        assert Action.MOVE(Direction.UP()) != Action.MOVE(Direction.LEFT())
+
 
 class TestBasicSerde:
     @staticmethod
@@ -39,3 +44,70 @@ class TestBasicSerde:
         assert Direction.from_primitive('d') == Direction.DOWN()
         assert Direction.from_primitive('l') == Direction.LEFT()
         assert Direction.from_primitive('r') == Direction.RIGHT()
+
+    @staticmethod
+    def test_point():
+        assert Point(3, 5).to_primitive() == [3, 5]
+        assert Point(0, 0).to_primitive() == [0, 0]
+        assert Point.from_primitive([5, 2]) == Point(5, 2)
+
+    @staticmethod
+    def test_snake_state():
+        assert SnakeState(head=Point(1, 2), tail=[]).to_primitive() == {'head': [1, 2], 'tail': []}
+        try:
+            assert SnakeState.from_primitive({
+                'head': [3, 4],
+                'tail': [],
+            }) == SnakeState(head=Point(3, 4), tail=[])
+        except Exception as e:
+            print(repr(e))
+            raise
+
+        assert SnakeState(
+            head=Point(3, 7),
+            tail=[Direction.UP(), Direction.UP(), Direction.RIGHT()],
+        ).to_primitive() == {'head': [3, 7], 'tail': ['u', 'u', 'r']}
+
+        assert SnakeState.from_primitive({
+            'head': [8, 5],
+            'tail': ['l', 'd', 'r', 'r'],
+        }) == SnakeState(
+            head=Point(8, 5),
+            tail=[Direction.LEFT(), Direction.DOWN(), Direction.RIGHT(), Direction.RIGHT()],
+        )
+
+    @classmethod
+    def test_field_state():
+        snake1 = SnakeState(head=Point(4, 2), tail=[Direction.LEFT(), Direction.UP()]),
+        snake2 = SnakeState(head=Point(8, 4), tail=[Direction.DOWN()]),
+        snake1p = snake1.to_primitive()
+        snake2p = snake2.to_primitive()
+        obj1 = (Point(5, 6), Object.FOOD())
+        obj2 = (Point(9, 3), Object.FOOD())
+        obj3 = (Point(1, 1), Object.FOOD())
+        obj1p = obj1.to_primitive()
+        obj2p = obj2.to_primitive()
+        obj3p = obj3.to_primitive()
+
+        assert FieldState(
+            snakes=[snake1, snake2],
+            objects=[obj1, obj2, obj3],
+        ).to_primitive() == {'snakes': [snake1p, snake2p], 'objects': [obj1p, obj2p, obj3p]}
+
+        assert FieldState.from_primitive({
+            'snakes': [snake1p, snake2p],
+            'objects': [obj1p, obj2p, obj3p],
+        }) == FieldState(
+            snakes=[snake1, snake2],
+            objects=[obj1, obj2, obj3],
+        )
+
+    @classmethod
+    def test_object():
+        assert Object.FOOD().to_primitive() == 'f'
+        assert Object.from_primitive('f') == OBJECT.FOOD()
+
+    @classmethod
+    def test_action():
+        assert Action.MOVE(Direction.LEFT()).to_primitive() == ['m', 'l']
+        assert Action.from_primitive(['m', 'd']) == Action.MOVE(Direction.DOWN())
