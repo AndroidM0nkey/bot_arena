a9eb293f766b6b5c2f7990b4d855a4ba77f9f702
Alexander Korzun <sahhash33@gmail.com>
2020-12-22 03:14:54 +0300

    Add documentation for message.py

--- bot_arena_proto/bot_arena_proto/message.py
+++ bot_arena_proto/bot_arena_proto/message.py
@@ -15,6 +15,45 @@ from typing import Type, cast
 
 @adt
 class Message(PrimitiveSerializable):
+    """A message, a low-level unit of information that is exchanged
+    between a client and a server.
+
+    Currently, the following types of messages exist:
+
+    - CLIENT_HELLO:     The message sent by a client when it connects
+                        to the server. The name of the player is the
+                        only parameter.
+
+    - SERVER_HELLO:     Successful server's response to a CLIENT_HELLO.
+
+    - YOUR_TURN:        A notification sent to a client that their turn
+                        has come and they can perform an action (see
+                        bot_arena_proto.data.Action and Message.ACT
+                        for details).
+
+    - READY:            A notification sent by a client to the server
+                        that the client is ready to begin the game.
+
+    - NEW_FIELD_STATE:  A notification from a server to a client that
+                        the field state has changed. The new field state
+                        is attached as a parameter.
+
+    - ACT:              An instruction sent by a client to the server
+                        that this client wants to perform a certain
+                        action (see bot_arena_proto.data.Action) with
+                        their snake. Only meaningful if it is this
+                        client's turn now.
+
+    - OK:               A generic server's response to some client's
+                        messages indicating that they were handled
+                        without an error. Clients can safely ignore this
+                        type of messages.
+
+    - ERR:              A generic server's response to some client's
+                        messages indicating that these messages were not
+                        handled properly due to an error. A description
+                        of the error is attached as a parameter.
+    """
     CLIENT_HELLO: Case[str]
     SERVER_HELLO: Case
     YOUR_TURN: Case
