8fbff4f81f60fabcf73c55d9af37bb9cf109cd7f
Alexander Korzun <sahhash33@gmail.com>
2021-01-06 16:16:18 +0300

    Add the skeleton for the server

---
+++ server/bot_arena_server/__main__.py
@@ -0,0 +1,11 @@
+__all__ = [
+    'main',
+]
+
+
+def main():
+    print('Hello world!')
+
+
+if __name__ == '__main__':
+    main()
---
+++ server/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name = 'bot-arena-server',
+    version = '0.1.0-a',
+    packages = find_packages(),
+
+    # Enable type hints in the installed package
+    package_data = {'bot_arena_server': ['py.typed']},
+
+    install_requires = ['bot-arena-proto'],
+)
