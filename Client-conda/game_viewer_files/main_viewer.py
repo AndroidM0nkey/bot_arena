33754c6fb6b4c33953677f6314e24f44eb2bb9d0
nnv-nick <niknas02@mail.ru>
2021-04-20 23:56:41 +0300

    Axis bug fixed

--- Client-conda/game_viewer_files/main_viewer.py
+++ Client-conda/game_viewer_files/main_viewer.py
@@ -7,6 +7,10 @@ import game_viewer_files.config as c
 import pygame
 
 
+def invert(p: int):
+    return (c.screen_width / c.cell_width) - 1 - p
+
+
 def get_message_and_display(cur_state: FieldState, surface: pygame.display):
     player_colors = [(0, 255, 0), (0, 0, 255)]  # GREEN and BLUE
     colors_cnt = -1
@@ -15,7 +19,7 @@ def get_message_and_display(cur_state: FieldState, surface: pygame.display):
         colors_cnt += 1
         snake_peaces = []
         cur_x = snake_state.head.x * c.cell_width
-        cur_y = snake_state.head.y * c.cell_width
+        cur_y = invert(snake_state.head.y) * c.cell_width
         snake_peaces.append(SnakeBodyPeace(cur_x, cur_y, player_colors[colors_cnt]))
         for peace in snake_state.tail:
             if peace == Direction.UP():
@@ -30,7 +34,7 @@ def get_message_and_display(cur_state: FieldState, surface: pygame.display):
         cur_snake = Snake(snake_peaces)
         cur_snake.draw(surface)
     for i in range(len(cur_state.objects)):
-        apple = Apple(cur_state.objects[i][0].x * c.cell_width, cur_state.objects[i][0].y * c.cell_width)
+        apple = Apple(cur_state.objects[i][0].x * c.cell_width, invert(cur_state.objects[i][0].y) * c.cell_width)
         apple.draw(surface)
     pygame.display.update()
     return
