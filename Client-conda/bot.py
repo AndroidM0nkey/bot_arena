e1028d6db016ea514e9b78bd106c42f8b202ecac
Valery Berezovsky <valerygegelevsky@yandex.ru>
2021-04-20 20:56:44 +0300

    Fix a lot of bugs and it almost works

--- Client-conda/bot.py
+++ Client-conda/bot.py
@@ -25,60 +25,60 @@ class Bot:
         apple = field_state.objects[0][0]
         """Координаты яблока."""
 
-        matrix[apple.x][apple.y] = -1
-
         for snake_state in field_state.snakes.values():
             x = snake_state.head.x
             y = snake_state.head.y
-            matrix[x][y] = -1
+            matrix[y][x] = -1
 
             """Идем по всем клеткам змейки, начиная с головы, и записываем в matrix -1."""
 
             for snake_cell in snake_state.tail:
                 if snake_cell == Direction.UP():
-                    x += 1
-                if snake_cell == Direction.DOWN():
-                    x -= 1
-                if snake_cell == Direction.LEFT():
-                    y -= 1
-                if snake_cell == Direction.RIGHT():
                     y += 1
-                matrix[x][y] = -1
+                if snake_cell == Direction.DOWN():
+                    y -= 1
+                if snake_cell == Direction.LEFT():
+                    x -= 1
+                if snake_cell == Direction.RIGHT():
+                    x += 1
+                matrix[y][x] = -1
 
         return (matrix, distances)
 
     def is_cell_correct(self, point, n, m):
         """Проверка на то, что координаты клетки помещаются в наше поле."""
 
-        return point[0] > -1 and point[0] < n and point[1] > -1 and point[1] < m
+        return point[0] > -1 and point[0] < m and point[1] > -1 and point[1] < n
 
     def bfs(self, matrix, distances, n, m, start_point):
         """Реализует алгоритм обхода в ширину, начиная со start_point."""
 
         """Добавляем стартовую вершину в очередь."""
         bfs_queue = deque()
-        bfs_queue.append((start_point.x, start_point.y))
-        distances[start_point.x][start_point.y] = 0
+        bfs_queue.append((start_point.y, start_point.x))
+        distances[start_point.y][start_point.x] = 0
 
         """Алгоритм продолжает работу, пока не кончились непосещенные вершины."""
 
         while len(bfs_queue) != 0:
             cell = bfs_queue.popleft()
-            x = cell[0]
-            y = cell[1]
+            y = cell[0]
+            x = cell[1]
             for move in Bot.moves:
                 """Считаем координаты соседней вершины."""
-                next_cell = (x + move[0], y + move[1])
-                next_x = next_cell[0]
-                next_y = next_cell[1]
+                next_cell = (y + move[0], x + move[1])
+                next_y = next_cell[0]
+                next_x = next_cell[1]
+                
 
                 """Проверяем, что такая клетка есть, свободна, и мы в ней еще не были,
                 и добавляем в очередь, подсчитывая расстояние."""
+
                 if self.is_cell_correct(next_cell, n, m):
-                    if matrix[next_x][next_y] == 0:
-                        if distances[next_x][next_y] == Bot.inf:
-                            distances[next_x][next_y] = distances[x][y] + 1
-                            bfs_queue.append((next_x, next_y))
+                    if matrix[next_y][next_x] == 0:
+                        if distances[next_y][next_x] == Bot.inf:
+                            distances[next_y][next_x] = distances[x][y] + 1
+                            bfs_queue.append((next_y, next_x))
 
         return distances
 
@@ -87,6 +87,9 @@ class Bot:
 
         """Посчитаем расстояния от яблока до каждой свободной клетки поля."""
         matrix, distances = self.field_to_matrix(field_state, n, m)
+        for st in matrix:
+            print(st)
+        
         apple = field_state.objects[0][0]
         distances = self.bfs(matrix, distances, n, m, apple)
 
@@ -99,29 +102,31 @@ class Bot:
         """Попробуем сделать ходы во все свободные клетки, соседние с головой змеи."""
 
         for move in Bot.moves:
-            next_cell = (x + move[0], y + move[1])
-            next_x = next_cell[0]
-            next_y = next_cell[1]
+            next_cell = (y + move[0], x + move[1])
+            next_y = next_cell[0]
+            next_x = next_cell[1]
             if self.is_cell_correct(next_cell, n, m):
-                if matrix[next_x][next_y] == 0:
+                if matrix[next_y][next_x] == 0 and distances[next_y][next_x] != Bot.inf:
 
                     """Если клетка внутри таблица и свободна, 
                         положим в список расстояние от нее до яблока и нужное 
                             направление."""
-
                     if (move == (0, 1)):
-                        ans.append((distances[next_x][next_y], Direction.RIGHT()))
+                        ans.append((distances[next_y][next_x], Direction.RIGHT()))
                     if (move == (0, -1)):
-                        ans.append((distances[next_x][next_y], Direction.LEFT()))
+                        ans.append((distances[next_y][next_x], Direction.LEFT()))
                     if (move == (1, 0)):
-                        ans.append((distances[next_x][next_y], Direction.UP()))
+                        ans.append((distances[next_y][next_x], Direction.UP()))
                     if (move == (-1, 0)):
-                        ans.append((distances[next_x][next_y], Direction.DOWN()))
+                        ans.append((distances[next_y][next_x], Direction.DOWN()))
 
+        
         """Если змейке некуда ходить, возвращаем произвольный ход и проигрываем:"""
         if len(ans) == 0:
             return Direction.UP()
 
         """Возвращаем направление хода в сторону клетки, от которой минимальное расстояние до клетки с яблоком."""
+
+        print (min(ans, key=lambda i: i[0]))
 
         return min(ans, key=lambda i: i[0])[1]
