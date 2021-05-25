f2d3836e25516f4e32163be1cb347e9f9602d084
Valery Berezovsky <valerygegelevsky@yandex.ru>
2021-05-26 02:04:50 +0300

    Rewrite the code of new bot

--- Client-conda/cppbots/newbot.cpp
+++ Client-conda/cppbots/newbot.cpp
@@ -5,136 +5,155 @@ using namespace std;
 
 #define endl '\n'
 
-int main(){
-    int n, m, num_snake, num_head;
-    cin >> n >> m >> num_snake >> num_head;
-    vector<vector<int>> field(n, vector<int>(m));
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++){
-            cin >> field[i][j];
-        }
-    }
 
+class BotAlgorithmA{
+private:
+    int n, m;
+    int num_snake, num_head;
 
-    pair<int, int> head;
+    pair<int,int> head;
+    vector<vector<int>> field;
+    const vector<pair<int,int>> moves = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+    map<const pair<int,int>, int> directions = 
+        {{{0, 1}, 1}, {{0, -1}, 3}, {{-1, 0}, 0}, {{1, 0}, 2}};
+public:
 
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++){
-            if (field[i][j] == num_head){
-                head = {i, j};
+    void input(){
+        cin >> n >> m >> num_snake >> num_head;
+        field.assign(n, vector<int>(m));
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                cin >> field[i][j];
             }
         }
     }
-    
-    vector<vector<int>> dists(n, vector<int>(m, -1));
-    vector<vector<int>> blocks(n, vector<int>(m, -1));
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++){
-            int number = 0;
-            for (int k = i - i % 10; k < min(n, i + (10 - i % 10)); k++){
-                for (int u = j - j % 10; u < min(m, j + (10 - j % 10)); u++){
-                    if (field[k][u] > 1 && field[k][u] != num_head && field[k][u] != num_snake){
-                        number++;
-                    }
-                }
-            }
-            blocks[i][j] = number;
-            //cout << blocks[i][j] << " ";
-        }
-        //cout << endl;
-    }
 
-    
-
-    vector<pair<int,int>> moves = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
-    set<pair<int, vector<pair<int,int>>>> s;
-    set<pair<int,int>> used;
-    vector<pair<int,int>> start = {head};
-    s.insert({4 * blocks[head.first][head.second], start});
-    //s.insert({0, start});
-
-    while (!s.empty()){
-        auto vertex = *s.begin();
-        s.erase(s.begin());
-        pair<int,int> ver = vertex.second.back();
-        //cout << ver.first + 1 << " " << ver.second + 1 << " " << vertex.first << endl;
-        if (field[ver.first][ver.second] == 1){
-            for (auto move : moves){
-                pair<int,int> next = {head.first + move.first, head.second + move.second};
-                if (next == vertex.second[1]){
-                    if (move == moves[0]){
-                        cout << 1 << endl;
-                    }
-                    if (move == moves[1]){
-                        cout << 3 << endl;
-                    }                
-                    if (move == moves[2]){
-                        cout << 0 << endl;
-                    }                
-                    if (move == moves[3]){
-                        cout << 2 << endl;
-                    }  
-                    //cout << "APPPLE " << vertex.second[1].first + 1 << " " << vertex.second[1].second + 1<< endl;
-                    return 0;
+    void findHead(){
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if (field[i][j] == num_head){
+                    head = {i, j};
                 }
             }
         }
+    }
+
+    int number_near_snakes(int i, int j){
+        int number = 0;
+        for (int k = max(0, i - 10); k < min(n, i + 10); k++){
+            for (int u = max(0, j - 10); u < min(m, j + 10); u++){
+                if (field[k][u] > 1 && field[k][u] != num_head 
+                                    && field[k][u] != num_snake){
+                    number++;
+                }
+            }
+        }
+        return number;
+    }
+
+    vector<vector<int>> calculateBlocks(){
+        vector<vector<int>> blocks(n, vector<int>(m, -1));
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                blocks[i][j] = number_near_snakes(i, j);
+            }
+        }
+        return blocks;
+    }
+    
+    bool isCellCorrect(pair<int,int> cell){
+        return cell.first > -1 && cell.first <  n 
+            && cell.second > -1 && cell.second < m;
+    }
+
+    vector<pair<int,int>> makeMoves(pair<int,int> cell){
+        vector<pair<int,int>> next_cells;
         for (auto move : moves){
-            pair<int,int> next = {ver.first + move.first, ver.second + move.second};
-            if (next.first > -1 && next.first < n && next.second > -1 && next.second < m){
-                if (field[next.first][next.second] <= 1){
-                    if (used.find(next) == used.end()){
-                        used.insert(next);
-                        int new_dist = vertex.first + 1;
-                        vector<pair<int, int>> new_vec = vertex.second;
-                        new_vec.push_back(next);
-                        s.insert({new_dist + 4 * blocks[next.first][next.second], new_vec});
-                        //s.insert({new_dist, new_vec});
-                    }
-                }
+            pair<int,int> next_cell = {cell.first + move.first, 
+                                cell.second + move.second};
+            if (isCellCorrect(next_cell)){
+                next_cells.push_back(next_cell);
             }
         }
+        return next_cells;
     }
-    
- 
-    
-    for (auto move : moves){
-        pair<int,int> next = {head.first + move.first, head.second + move.second};
-        if (next.first > -1 && next.first < n && next.second > -1 && next.second < m){
-            if (field[next.first][next.second] == 0){
-                if (move == moves[0]){
-                    cout << 1 << endl;
-                }
-                if (move == moves[1]){
-                    cout << 3 << endl;
-                }                
-                if (move == moves[2]){
-                    cout << 0 << endl;
-                }                
-                if (move == moves[3]){
-                    cout << 2 << endl;
-                }                
-                return 0;
+
+    bool goToEmptyCell(pair<int, int> cell){
+        auto next_cells = makeMoves(cell);
+        for (auto next_cell : next_cells){
+            if (field[next_cell.first][next_cell.second] == 0){
+                pair<int,int> move = {next_cell.first - cell.first, 
+                        next_cell.second - cell.second};
+                cout << directions[move] << endl;   
+                return true; 
             }
         }
-    }    
+        return false;
+    }
 
-    cout << 0 << endl;
 
+    void findDirection(){
+        findHead();
+        vector<vector<int>> blocks = calculateBlocks();
+    
+
+        set<pair<int, vector<pair<int,int>>>> s;
+        set<pair<int,int>> used;
+        vector<pair<int,int>> start = {head};    
+        s.insert({blocks[head.first][head.second], start});
+
+
+        while (!s.empty()){
+            auto element = *s.begin();
+            s.erase(s.begin());
+
+            int distance = element.first;
+            vector<pair<int,int>> cur_path = element.second;
+            pair<int,int> vertex = element.second.back();
+            
+            if (field[vertex.first][vertex.second] == 1){
+                pair<int,int> next_cell = cur_path[1];
+                pair<int,int> move = {next_cell.first - head.first, 
+                        next_cell.second - head.second};
+                cout << directions[move] << endl;
+                return;
+            }
+
+            auto next_cells = makeMoves(vertex);
+
+            for (auto next_cell : next_cells){
+                if (field[next_cell.first][next_cell.second] <= 1 && 
+                                used.find(next_cell) == used.end()) {
+                    used.insert(next_cell);
+                    vector<pair<int, int>> new_path = cur_path;
+                    new_path.push_back(next_cell);
+                    s.insert({distance + 1 + blocks[next_cell.first][next_cell.second], new_path});
+                }
+            } 
+        }
+
+        if (!goToEmptyCell(head)){;
+            cout << 0 << endl;
+        }
+        return;
+    }
+ 
+};
+
+int main(){
+    BotAlgorithmA bot;
+    bot.input();
+    bot.findDirection();
     return 0;
 }
 
 /*
 
-6 6
+4 4
 2 3
-
-0 0 0 0 0 0
-0 0 0 0 0 0
-0 0 0 2 2 0
-1 0 0 3 2 0
-0 0 0 0 0 0
-0 0 0 0 0 0
-
+0 0 0 2
+0 0 0 3
+0 0 0 0
+0 0 0 1
 
 */
