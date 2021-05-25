1b679c149428092ae24bcacae6a274a73fa4d0cb
Valery Berezovsky <valerygegelevsky@yandex.ru>
2021-05-26 02:37:14 +0300

    Finish agressivebot (python)

--- Client-conda/pythonbots/agressivebot.py
+++ Client-conda/pythonbots/agressivebot.py
@@ -5,49 +5,42 @@ from pythonbots.matrixbfs import MatrixBfs
 
 class AgressiveBot(Bot):
     def find_direction(self, field_state: FieldState, m: int, n: int, snake_name: str) -> Direction:
-        """Принимает на вход поле, имя змеи и выдает направление хода."""
+        """Принимает на вход поле, его размеры, имя змеи и выдает направление хода."""
 
-        """Посчитаем расстояния от яблока до каждой свободной клетки поля."""
+        """Переведем поле в матрицу."""
         matrix = self.field_to_matrix(field_state, n, m)
         
+        """Посчитаем расстояния от каждой клетки поля до голов соперников."""
         start_points = []
-        
         for snake_state in field_state.snakes.values():
             if snake_state != field_state.snakes[snake_name]:
                 x, y = snake_state.head.y, snake_state.head.x
                 start_points.append((x, y))
-
         bfs = MatrixBfs()
         distances = bfs.bfs(matrix, n, m, start_points)
-        print(distances)
+
+        """Попробуем сделать ходы во все свободные клетки, соседние с головой змеи."""
 
         snake_state = field_state.snakes[snake_name]
         x, y = snake_state.head.y, snake_state.head.x
-
         ans = []
         moves = ((0, 1), (0, -1), (1, 0), (-1, 0))
+        directions = {(0, 1): Direction.RIGHT(), (0, -1):Direction.LEFT(), (1, 0):Direction.UP, (-1, 0):Direction.DOWN()}
 
         """Попробуем сделать ходы во все свободные клетки, соседние с головой змеи."""
         for move in moves:
             new_cell = (x + move[0], y + move[1])
             new_x, new_y = new_cell[0], new_cell[1]
+            """Если клетка внутри таблица и свободна, положим в список расстояние 
+                от нее до яблока и нужное направление."""
+
             if bfs.is_cell_correct(new_cell, n, m) and matrix[new_x][new_y] == 0:
-                """Если клетка внутри таблица и свободна, 
-                    положим в список расстояние от нее до яблока и нужное 
-                        направление."""
-                if (move == (0, 1)):
-                    ans.append((distances[new_x][new_y], Direction.RIGHT()))
-                if (move == (0, -1)):
-                    ans.append((distances[new_x][new_y], Direction.LEFT()))
-                if (move == (1, 0)):
-                    ans.append((distances[new_x][new_y], Direction.UP()))
-                if (move == (-1, 0)):
-                    ans.append((distances[new_x][new_y], Direction.DOWN()))
+                ans.append(distances[new_x][new_y], directions[move])
         
         """Если змейке некуда ходить, возвращаем произвольный ход и проигрываем:"""
         if len(ans) == 0:
             return Direction.UP()
 
-        """Возвращаем направление хода в сторону клетки, от которой минимальное расстояние до клетки с яблоком."""
+        """Возвращаем направление хода в сторону клетки, от которой минимальное расстояние до ближайшей головы соперника."""
 
         return min(ans, key=lambda i: i[0])[1]
