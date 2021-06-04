2538d29e1b3916bc78bf8dde5ee6d51210329bbc
Alexander Korzun <sahhash33@gmail.com>
2021-06-04 17:01:46 +0300

    Fix a small bug in bot_arena_proto.

--- bot_arena_proto/setup.py
+++ bot_arena_proto/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'bot_arena_proto',
-    version = '1.0.0',
+    version = '1.0.1',
     packages = find_packages(),
 
     # Enable type hints in the installed package
--- server-gui/setup.py
+++ server-gui/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'bot-arena-server-gui',
-    version = '0.1.0',
+    version = '1.0.0',
     packages = find_packages(),
 
     # Enable type hints in the installed packages
